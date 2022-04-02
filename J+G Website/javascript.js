0178e4d0102e67753339f5744c2bb5966228f5f5
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-02 13:39:15 +0300

    Javascript function checks for number vs text

--- J+G Website/javascript.js
+++ J+G Website/javascript.js
@@ -1,13 +1,21 @@
 function add_numbers(){
-    var num1 = Number(document.getElementById("number1").value);
-    var num2 = Number(document.getElementById("number2").value);
+    
+    var num1 = document.getElementById("number1").value;
+    var num2 = document.getElementById("number2").value;
+    var output;
+    var output_string;
+    if (isNaN(num1)  && isNaN(num2))
+    {
+        output_string = "Please input a valid number";
+     }
+     else{
+        output = Number(num1) + Number(num2);
+        output_string = "The sum of " + num1 + " and " + num2 + " is " + output;
+     }
     // Fetch num1 from input box here
     // Fetch num2 from input box here
 
-    var output = num1+num2;
-
-    var output_string;
-    output_string = "The sum of " + num1 + " and " + num2 + " is " + output;
+    
     // Set output label text to output_string here
     document.getElementById("sum").innerHTML= output_string;
 }
