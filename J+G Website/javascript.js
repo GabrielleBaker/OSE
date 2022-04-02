b1ddd0d5f0c4a5fc330185d370e1bfa7ba478d5e
Jaryd Baker <jarydbaker@gmail.com>
2022-04-02 13:16:16 +0300

    Created basic functionality. Need to add code to fetch the relevant numbers from the HTML input box and to update the HTML label with the resultant string. Need assistance with this.

---
+++ J+G Website/javascript.js
@@ -0,0 +1,12 @@
+function add_numbers(){
+    var num1;
+    var num2;
+    // Fetch num1 from input box here
+    // Fetch num2 from input box here
+
+    var output = num1+num2;
+
+    var output_string;
+    output_string = "The sum of " + num1 + " and " + num2 " is " + output;
+    // Set output label text to output_string here
+}
