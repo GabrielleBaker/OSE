c4825df34361f7dd8bd158651a97151b11372fde
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-02 13:22:24 +0300

    Javascript input linked from html

--- J+G Website/javascript.js
+++ J+G Website/javascript.js
@@ -1,6 +1,6 @@
 function add_numbers(){
-    var num1;
-    var num2;
+    var num1 = Number(document.getElementById("number1").value);
+    var num2 = Number(document.getElementById("number2").value);
     // Fetch num1 from input box here
     // Fetch num2 from input box here
 
@@ -9,4 +9,5 @@ function add_numbers(){
     var output_string;
     output_string = "The sum of " + num1 + " and " + num2 + " is " + output;
     // Set output label text to output_string here
+    document.getElementById("sum").innerHTML= output_string;
 }
