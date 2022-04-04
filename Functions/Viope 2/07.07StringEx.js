793322d93292d3da1d553a93ece17a0b81b25ae3
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-04 12:38:08 +0300

    New function created

--- Functions/Viope 2/07.07StringEx.js
+++ Functions/Viope 2/07.07StringEx.js
@@ -1,22 +1,27 @@
 function processName() {
-
-    // read value of name from the input field
-   
-
-    // show it with "ALL CAPS" using toUpperCase()
-   
-
-    // show it with  "all small letters" using toLowerCase()
- 
-
-    //tell how many characters are there (length, includes also all spaces) 
-   
-
-    // tell, whether the input string contains the word 'muumi'
+    //get the input
+    var name = document.getElementById("name").value;
+    //change name to uppercase
+    document.getElementById("upperCase").innerHTML = "With upper case letters: " + name.toUpperCase();
+    //change name to lowercase
+    document.getElementById("lowerCase").innerHTML = "With lower case letters: " + name.toLowerCase();
+    //check length of name inlc white space
+    document.getElementById("length").innerHTML = "Character count: " + name.length;
     
-    // tell the first character using charAt()
-    
+    var muumiIndex;
+    //check if muumi exists in text and use the output of indexOf to generate correct response
+if(name.indexOf('muumi') === -1){
+    muumiIndex = "Doesn't contain the text muumi."
+}  
+else{
+    muumiIndex = "Contains the text muumi."
+}
+    //print index of answer in human readable response to html
+    document.getElementById("contains").innerHTML = muumiIndex;
+    //check first character, remember it starts from zero
+    document.getElementById("firstCharacter").innerHTML = "First character: " + name.charAt(0);
 
+    document.getElementById("manyCharacters").innerHTML = "First three characters: " + name.substr(0, 3);
     // tell which ones are the first three characters using subStr().
    
 }
