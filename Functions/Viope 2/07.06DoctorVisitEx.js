bfa7314b4fd8a3780cd7338f658877fd8c1e2b92
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-04 12:07:05 +0300

    new functions created

--- Functions/Viope 2/07.06DoctorVisitEx.js
+++ Functions/Viope 2/07.06DoctorVisitEx.js
@@ -1,16 +1,49 @@
-// Defne a function (1.) 
-
-// Defne a function (2.) 
+// this function gets the length of the visit to determine max reimbursion from Kela 
+function doctorVisit (length){
+	const parsed=parseInt(length);
+	var kelaReimbursement ;
+if(isNaN(parsed)) { return 0; }
 	
+else if(length > 0 && length <= 10){
+	kelaReimbursement = 8;
+}
+else if(length > 10 && length <= 20){
+	kelaReimbursement = 11;
+}
+else if( length> 20 && length <= 30){
+	kelaReimbursement = 13.50
+}
+else if( length > 30 && length <= 45){
+	kelaReimbursement = 16.50;
+}
+else{
+	kelaReimbursement= 21;
+}
+return Number(kelaReimbursement);
+}
+// this function calculates what is to be paid by the customer, drs fee - kela reimbursement + office fee
+function customersBill (fee, reimbursement){
+	var officeFee = 15.90;
+	var toPay = fee - reimbursement + officeFee;
+	return toPay;
+}	
 	
 // Define a function (3.) 
 function calculate() {
-		// Read values from the input fields
-		
-		// Call the function (1.) which calculates Kela reimbursement 
-		
-		// Call the function (2.) which calculates amount left for the customer to pay
-		
-		// Write the answer on the page, to the answer div, as content of the div
-		
+	//lengthofvisit gets the input field from user
+var lengthOfVisit = Number(document.getElementById("length").value);	
+	//doctors fee gets input
+var doctorFee = Number(document.getElementById("doctorsFee").value);
+ //officefee is fixed
+var officeFee = 15.90;
+//visit returns the kela reimbursement based on the length of the drs visit
+var visit = doctorVisit(lengthOfVisit);
+//bills determines the bill for customer to pay
+var bills = customersBill(doctorFee,visit);
+
+document.getElementById("answer").innerHTML = 
+"Doctor's fee is " + doctorFee.toFixed(2)	+ " euros. <br>" + 
+"Kela reimbursement is " + visit.toFixed(2) + " euros. <br>" +
+"Office fee is " + officeFee.toFixed(2) + " euros. <br>" +
+ "Customer needs to pay " + bills.toFixed(2) + " euros.";
 }
