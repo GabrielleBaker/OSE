1460bf16789cdde10304b57b56b8c04897e6f5da
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-04 13:16:48 +0300

    new functions

--- Functions/Viope 2/07.09AgeEx.js
+++ Functions/Viope 2/07.09AgeEx.js
@@ -1,20 +1,21 @@
 function tellAge() {
     // Read value of yearOfBirth from the input field 
-   
+   var yob = document.getElementById("yearOfBirth").value;
 
     // Figure out the current date into today variable 
-  
-
+    var today = new Date();
     // Use getFullYear() method to get the current year out from the today variable
-   
+   var year = today.getFullYear();
 
     // Calculate the (rough estimate of the) age
+    var age = year - yob;
+    var output;
+    if(age < 0){
+        output = "Please enter an accurate date of birth.<br>"
+    }
+    else{
+        output = "You are now " + age + " years old. <br>"
+    }
+    document.getElementById("answer").innerHTML = output + "(Current year is " + year + ".)";
     
-
-    // If age is less than zero
- 
-        //  Show an error message
-        
-    // otherwise tell the age
-   
 }
