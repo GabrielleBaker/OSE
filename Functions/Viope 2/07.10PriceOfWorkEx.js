6daba7820d9511567340e8d11f6e8440092745bc
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-04 16:07:20 +0300

    new functions

--- Functions/Viope 2/07.10PriceOfWorkEx.js
+++ Functions/Viope 2/07.10PriceOfWorkEx.js
@@ -1,52 +1,74 @@
 function modifyTitle() {
-
-    // Read value of title from the input field
-    // Call allCapsTitleTrimmed() function 
-
-
+    
+	var inputTitle = document.getElementById("title").value;
+	allCapsTitleTrimmed(inputTitle);
+    
 }
 
 function allCapsTitleTrimmed(text) {
 	// Removes any empty white space from the beginning or the end of the text
-    
+    var removeWhiteSpace = text.trim();
 	// Changes the text given to it to be ALL CAPS
-    
-	// Return the changed text
-	
+    var newTitle = removeWhiteSpace.toUpperCase();
+	// Return the changed text back to input field
+	title.value = newTitle;
 }
 
 function calculate() {
+  //get input from html
+  var dateText = document.getElementById("date").value;
+  //call second function
+  var sunday = isSunday(dateText);
+  var price ;
+  var day;
+  //if sunday is marked true from 2nd function, then give sunday price
+  if(sunday === true){
+	  price = 72;
+	  day = "Sunday";
+  }
+  //else use weekday prices
+  else{
+	  price = 48;
+	  day = "Work day";
+  }
+  //check start and end time from input
+var startTime = document.getElementById("startTime").value;
+var endTime =document.getElementById("endTime").value;
 
-    // Calculate length of the work as minutes.
-
-    // If it is not Sunday  (Call isSunday() function)
-        //Calculate and show price of the repair work during the workdays
-    //otherwise			
-        //Calculate and showprice of the repair work on Sundays
-
+var workHours = endTime.substr(0, 2) - startTime.substr(0 , 2);
+var workMinutes = endTime.substr(3,2) - startTime.substr(3 , 2);
+var outputMinutes = workHours*60 + workMinutes;
+var costOfLabour = (outputMinutes /60) * price; 
+  //print to html
+  document.getElementById("answer").innerHTML= "Length of the work was " + outputMinutes + " minutes. <br>" + 
+  "The hourly price is during the " + day + "s " + price.toFixed(2) + " euros.<br>" +
+  "The price of this repair work is " + costOfLabour.toFixed(2) + " euros.";
 }
 
  // The function gets the date text in the format "dd.MM.yyyy". 
  // The function returns Boolean value true if the date is for a Sunday, otherwise false.
 function isSunday(dateText) {
 	
-	// Split the given date text into day, month and year parts using the subStr() method
+    var day= dateText.substr(0, 2);
+    var month = dateText.substr(3, 2);
+    var year = dateText.substr(6, 4);
 	
+	var asDate = new Date();
+        asDate.setDate(day);
+        asDate.setMonth(month-1);
+        asDate.setFullYear(year);
 
-	// Create a new Date object to a variable asDate
-	
+    var dayNow = asDate.getDay();
+    var output ;
 
-	// Get the day of the week with its getDay() method. Sunday is number 0 .
-	
+    if(dayNow === 0){
+        output = true;
+    }
+    else{
+       output =  false;
+    }
+    return output;
 
-	// If day of the week is Sunday 
-	
-		// return Boolean value true
-		
-	// otherwise	
-	
-       // return Boolean value false		
-	
 }
 
 
