eb659368bf97d3d766431f29c83e903211410034
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-04 16:58:25 +0300

    new files and functions

--- Functions/RecapDemo_PlayersAndScores_0_EmptyTemplate (2)/RecapDemo2_EmptyTemplate/Step_0_Only_HTML_page/player_code.js
+++ Functions/RecapDemo_PlayersAndScores_0_EmptyTemplate (2)/RecapDemo2_EmptyTemplate/Step_0_Only_HTML_page/player_code.js
@@ -1,15 +1,54 @@
-﻿//var names = [];
-//var scores = [];
+﻿var names = [];
+var scores = [];
+    //push names and scores into respective arrays
+function pushArrays(){
+    names.push(document.getElementById("txtName").value);
+    scores.push(document.getElementById("txtScore").value);
+    txtName.value = "";
+    txtScore.value = "";
+}
 
-// ----------------------------------------------------------------------------
 
-// ----------------------------------------------------------------------------
+//display string of players' names onclick
+function listPlayers(){
+    //get string from array
+        var nameList= "";
+    for(i=0; i < names.length; i++){
+      nameList = nameList  + "<br>" + names[i];
+}
 
-// ----------------------------------------------------------------------------
+    document.getElementById("pOutput").innerHTML = "List of players: " + nameList;
+}
 
-// ----------------------------------------------------------------------------
+function Search(text){
+    var text = document.getElementById("inputname").value;
+    for (i=0; i < names.length; i++){
+        if (text = names[i]){
+            var name = names[i];
+            var score = scores[i];
+            var output = "Player: " + name + "<br> Score: " + score;
+        }
+        else{
+            output = "Insert a valid name to search the database";
+        }
+    }
+    inputname.value = "";
+    document.getElementById("pOutput2").innerHTML = output;
+}
 
-// ----------------------------------------------------------------------------
-
-// ----------------------------------------------------------------------------
-
+function winner(){
+    var currentScore= 0;
+    var nameOfWinner= "";
+    var output;
+    for(i = 0; i < scores.length; i++){
+        if( scores[i] > currentScore){
+            currentScore = scores[i];
+            nameOfWinner = names[i];
+            output = "The winner is: " + nameOfWinner + "<br> With a score of: " + currentScore;
+        }
+        else{
+            output = "There is no winner at present";
+        }
+    }
+    document.getElementById("pOutput3").innerHTML = output;
+}
