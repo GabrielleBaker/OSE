4fe6f8a5b70831ccf81f36627bb78654f896d808
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-06 16:10:07 +0300

    new files

--- Objects/viope/10.05CoursesEx.js
+++ Objects/viope/10.05CoursesEx.js
@@ -1,10 +1,31 @@
 // Create an object array about courses
-var courses = [
-    // INSERT CODE HERE
-];
-
+var courses = [ 
+    {  name: "Orientation to software engineering",
+        code   : "SWD1TF001",
+        extent : 5,
+        timing : "1.semester"
+          }, 
+    {  name: "Programming",
+        code   : "SWD1TF002",
+        extent : 5,
+        timing : "2.semester"
+          }, 
+    {  name: "Data management and databases",
+        code   : "SWD1TF003",
+        extent : 5,
+        timing : "2.semester"
+          }, 
+    {  name: "Mobile development project",
+        code   : "SWD1TF004",
+        extent : 10,
+        timing : "3.semester"
+          }
+    ];
+    console.log(courses);
 function listCourses() {
-    
+    for(i = 0; i <=courses.length; i++){
+        //var code = 
+    }
     // List courses from an object array , i is an index of array
     
     // Write the answer on the page, to the answer div, as content of the div
