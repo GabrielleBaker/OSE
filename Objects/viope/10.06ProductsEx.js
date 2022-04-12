e86181ac72c7629c844c234b8a9e4327c294e30d
GabrielleBaker <98098568+GabrielleBaker@users.noreply.github.com>
2022-04-12 17:26:33 +0300

    solutions

---
+++ Objects/viope/10.06ProductsEx.js
@@ -0,0 +1,16 @@
+// JSON form
+var json = '[{"number": 1, "name": "Samsung Smart LED-TV", "price": 449.00, "description": "HD-digi ready"},' + '{"number": 2, "name": "Samsung 2.1 SoundBar Home Theater", "price": 198.00, "description": "Connect to TV and stream music wireless"},' + '{"number": 3, "name": "Dilog CI+/CA module for pay-tv-cards", "price": 63.90, "description": "CI+ module for the table televisions"}]';
+
+var products = JSON.parse(json);
+// Parse the JSON text into Javascript objects (1 array, 3 'product' objects)
+
+var output = "";
+function listProducts() {
+   for(i = 0; i < products.length; i++){
+    output += "<p>" + "Number: " + products[i].number + "<br>" +
+     "Name: " + products[i].name + "<br>" + 
+     "Price: " + (products[i].price).toFixed(2) + "<br>" + 
+     "Description: " + products[i].description + "</p>" ; 
+   }
+document.getElementById("productList").innerHTML= output; 
+}
